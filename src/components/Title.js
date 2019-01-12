817bf74d9e5e781da70e4928db128a5bce46a0db
Filip Czabator <fczabator@gmail.com>
2019-01-12 19:47:01 +0100

    Initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import Typist from 'react-typist'
+
+const Text = styled.div`
+  font-size: 26px;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: 300;
+`
+
+export class TypistLabel extends React.Component {
+  render() {
+    return (
+      <Text>
+        <Typist startDelay={2000} avgTypingDelay={200}>
+          Filip Czabator
+        </Typist>
+      </Text>
+    )
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <Seo
+      title="Home"
+      keywords={[`filip`, `czabator`, `react`, `filipczabator`, `fczabator`]}
+    />
+  </Layout>
+)
+
+export default IndexPage
